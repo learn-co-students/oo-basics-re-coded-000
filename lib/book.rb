e8c5ed8efbd830358dc8a0f04edbcbd2991d0e31
nichervan brhik <nichervan.brhik@gmail.com>
2016-10-30 11:58:15 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,32 @@
 class Book
-
+  def initialize(title)
+  @title=title
+  end
+def title
+  @title
 end
+def author=(name)
+  @author=name
+end
+def author
+  @author
+end
+def page_count=(number)
+  @page_count=(number)
+end
+def page_count
+  @page_count
+end
+def genre=(genre)
+  @genre=(genre)
+end
+def genre
+  @genre
+end
+def turn_page
+  @turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+end
+book = Book.new("Some Title")
+book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,39 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand=(brand)
+  end
+  def brand=(nike)
+    @brand=(nike)
+  end
+  def brand
+    @brand
+  end
+  def color=(white)
+    @color=(white)
+  end
+  def color
+    @color
+  end
+  def size=(num)
+    @size=(num)
+  end
+  def size
+    @size
+  end
+  def material=(x)
+    @material=(x)
+  end
+  def material
+    @material
+  end
+  def condition=(new)
+    @condition=(new)
+  end
+  def condition
+    @condition
+  end
+  def cobble
+    @condition="new"
+    puts "Your shoe is as good as new!"
+  end
+end
